0a77f769e847211c853130d383b1677d4ed71f1a
beast-machine <animeshroy@live.com>
2022-12-18 17:41:54 +0530

    kitty 404
    Signed-off-by: beast-machine <animeshroy@live.com>

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,14 +6,8 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <img class="img" src="img/404.gif"></img>
+      <h1>404!!! cat not found</h1>
     </main>
   );
 }
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,9 +5,29 @@ export default function about() {
   return (
     <main>
       <Title>Phishing Landing Page</Title>
-     <center>
-        <h2><a href="https://twitter.com/anir0y">@anir0y</a></h2>
-     </center>
+      <p>
+      Phishing is a type of cyber attack that uses fraudulent emails, texts, or websites to trick people into disclosing sensitive information, such as login credentials or financial information. It is a major threat to both individuals and organizations, as it can result in the theft of sensitive data, the compromise of accounts, and even financial losses.
+
+      To protect yourself from phishing attacks, it is important to be aware of the common tactics used by attackers and to take steps to safeguard your information. Here are some tips to help you stay safe:
+
+      Be suspicious of unsolicited emails, texts, or messages: If you receive an unexpected message from someone you don't know, or from a company you do business with, be wary. Phishers often use fake email addresses or impersonate real ones to trick you into believing the message is legitimate.
+
+      Look for telltale signs of a phishing attack: These include misspellings and grammatical errors, threats or urgency, and links that lead to unfamiliar websites.
+
+      Don't click on links or download attachments from suspicious emails: If you receive an email or message with a link, hover your cursor over the link to see where it will take you before clicking. If the link looks suspicious, do not click on it. Similarly, do not download attachments from emails or messages you weren't expecting.
+
+      Use strong, unique passwords: Using strong, unique passwords for all of your accounts can help protect against phishing attacks. It's also a good idea to use a password manager to help you keep track of your passwords.
+
+      Enable two-factor authentication: Two-factor authentication (2FA) is an extra layer of security that requires you to provide a second form of authentication, such as a code sent to your phone or a biometric scan, in addition to your password. Enabling 2FA can help protect against phishing attacks and other types of cyber threats.
+
+      By following these tips and staying vigilant, you can help protect yourself and your organization from phishing attacks. Remember, if you receive a suspicious email or message, do not respond or click on any links. Instead, report the message to your IT team or the authorities.
+      </p>
+      <br />
+      <br />
+      <h4>written by : OPENGPT</h4>
+      
+     
+      <h2><a href="https://twitter.com/anir0y">@anir0y</a></h2>
     </main>
   );
 }
